f703e800329b39cfbde6a6e60cb1bbbe0fb81358
Chris Bandy <bandy.chris@gmail.com>
2013-07-17 23:22:37 -0500

    Print some parsed output of `df`

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+set -vx
 
 test -L /dev/shm && ( sudo rm /dev/shm && sudo mkdir /dev/shm && sudo mount -t tmpfs shmfs -o size=2G /dev/shm )
 test -f /sbin/chkconfig || ( echo '#!/bin/sh' | sudo tee /sbin/chkconfig > /dev/null && sudo chmod u+x /sbin/chkconfig )
