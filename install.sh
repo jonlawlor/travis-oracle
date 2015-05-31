34493d751f0c62519fbf5e08b7c8f3ae76c4c708
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-05-31 18:25:10 -0400

    reorder install

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ cd "$(dirname "$(readlink -f "$0")")"
 sudo apt-get -qq update
 sudo apt-get --no-install-recommends -qq install alien bc libaio1 unzip
 
-if [ -n "$ORACLE_FILE" ]; then
-	setup_oracle_xi.sh
-fi
-
 if [ -n "$ORACLE_CLIENT_FILES" ]; then
 	for client_file in ${ORACLE_CLIENT_FILES//:/ }; do
 		sudo dpkg --install `sudo alien --scripts --to-deb "$client_file" | cut -d' ' -f1`
 	done
 fi
-		
+
+if [ -n "$ORACLE_FILE" ]; then
+	setup_oracle_xi.sh
+fi
